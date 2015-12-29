20c6c0c29ce0b59143b59f4196946317d072f409
Sara Holm <Sara Holm>
2015-12-29 10:21:41 +0100

    db class & ads class

--- classes/ads.class.php
+++ classes/ads.class.php
@@ -2,8 +2,25 @@
 
 class Ads {
 
-	private $id, $title, $content, $dateCreated, $userId, $imageName;
+	private $id, $title, $content, $dateCreated, $userId, $imageName, $tags;
 
-	
+	function __construct() {
 
+	}
+
+	function getAllAds() {
+		$query = DB::query(
+			"SELECT title, text, date_created, user_id, address 
+			FROM ads, user 
+			WHERE user.id = ads.user_id" 
+		);
+	}
+
+	function getSpecificAd(){
+
+	}
+
+	function createAd() {
+
+	}
 }
