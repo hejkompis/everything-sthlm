65ea8bef68022363a8fcd5e1f6ecd7f5c1e73e28
Sara Holm <Sara Holm>
2015-12-29 11:03:41 +0100

    adding querys and static variable  to ads class

--- classes/ads.class.php
+++ classes/ads.class.php
@@ -3,21 +3,27 @@
 class Ads {
 
 	private $id, $title, $content, $dateCreated, $userId, $imageName, $tags;
+	static private $query; 
 
 	function __construct() {
 
 	}
 
-	function getAllAds() {
-		$query = DB::query(
+	static public function getAllAds() {
+		self::$query = DB::query(
 			"SELECT title, text, date_created, user_id, address 
 			FROM ads, user 
 			WHERE user.id = ads.user_id" 
 		);
 	}
 
-	function getSpecificAd(){
-
+	static public function getSpecificAd($id){
+		self::$query = DB::query (
+			"SELECT title, text, date_created, firstname, address
+			FROM ads, user
+			WHERE user.id = ads.user_id AND ads.id = $id", 
+			TRUE
+		);
 	}
 
 	function createAd() {
