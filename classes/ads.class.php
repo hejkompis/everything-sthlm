753cfc46a003a8c578ae8a59aa24364edbb42704
Sara Holm <Sara Holm>
2015-12-30 15:17:58 +0100

    Ads class updated w search function for string

--- classes/ads.class.php
+++ classes/ads.class.php
@@ -29,16 +29,23 @@ class Ads {
 		if (isset($input['id'])){ //annonsid
 			return self::getSpecificAd($input['id']);
 		} else { 
-			return self::getAllAds();
+			return self::getAllAds($input);
 		}
 	}
 
-	static public function getAllAds() {
-		
+	static public function getAllAds($input = FALSE) {
+		if($input['search']) {
+			$searchString = $input['search'];
+			$sqlSearch = " AND ads.title LIKE '%".$searchString."%' OR ads.content LIKE '%".$searchString."%' ";	
+		} else {
+			$searchString = FALSE;
+			$sqlSearch = "";
+		}
+
 		$data_array = DB::query(
 			"SELECT ads.id as id, ads.title as title, ads.content as content, ads.date_created as date_created, ads.user_id as user_id, user.address_zip as zipcode
 			FROM ads, user 
-			WHERE user.id = ads.user_id" 
+			WHERE user.id = ads.user_id".$sqlSearch 
 		);
 
 		foreach ($data_array as $data) {
@@ -48,7 +55,8 @@ class Ads {
 		$output = [
 		'ads' => $ads,
 		'page' => 'ads.getallads.twig',
-		'title' => 'Alla annonser'
+		'title' => 'Alla annonser',
+		'search' => $searchString
 		];
 
 		return $output;
