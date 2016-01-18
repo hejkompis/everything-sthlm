11952eab40338bcaf7fd1ae844c3b49ebd5d83bf
Sara Holm <Sara Holm>
2016-01-18 13:23:32 +0100

    stats functionality in home class

--- classes/home.class.php
+++ classes/home.class.php
@@ -6,10 +6,28 @@
 
 			$output = [
 				'page' => 'home.twig',
-				'user' => User::isLoggedIn(FALSE)
+				'user' => User::isLoggedIn(FALSE),
+				'activeadsamount' => self::getActiveAds() 
 			];
 
 			return $output;
+			//'usersamount' => self::getUsers()
+		}
+
+		static public function getActiveAds() {
+			$sql = "SELECT COUNT(id)
+					FROM ads
+					WHERE active = 1
+			"; 
+
+			$data = DB::query($sql);
+
+			$output = $data;
+
+			return $output;
+		}
+
+		static public function getUsers() {
 
 		}
 
