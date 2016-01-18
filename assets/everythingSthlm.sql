3cb9e087021f259918df51a3f7c14681b7f481a2
Per Olsson <per.olsson@gmail.com>
2016-01-18 10:18:41 +0100

    New db in /assets/

--- assets/everythingSthlm.sql
+++ assets/everythingSthlm.sql
@@ -21,20 +21,22 @@ CREATE TABLE `ads` (
   `payment` varchar(250) COLLATE utf8_bin NOT NULL,
   `active` int(11) NOT NULL,
   `image` varchar(200) COLLATE utf8_bin NOT NULL,
+  `latitude` varchar(200) COLLATE utf8_bin NOT NULL,
+  `longitude` varchar(200) COLLATE utf8_bin NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
-INSERT INTO `ads` (`id`, `title`, `content`, `address_street`, `address_zip`, `address_city`, `date_created`, `date_updated`, `date_expire`, `user_id`, `ad_type`, `payment`, `active`, `image`) VALUES
-(3, 'Test ad 3',  'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ornare lacinia sapien sed congue. Suspendisse dignissim eros non nisl rhoncus convallis. Sed consequat venenatis lobortis. Sed luctus auctor nisl ut posuere. Mauris tincidunt maximus risus, ac dignissim ipsum dignissim eu. Integer pharetra leo nec luctus cursus. Nunc ut lobortis nulla, ac pulvinar massa. Phasellus sit amet arcu a metus accumsan egestas id non ante. Aenean at massa dapibus, convallis quam in, cursus metus. Nunc venenatis ante eu congue porta. Praesent ac velit a massa viverra sollicitudin. Sed accumsan dolor sed turpis tincidunt facilisis. Phasellus ut rutrum tortor, quis volutpat odio. Donec porta purus nunc, in faucibus sem hendrerit eu.',  '', 0,  '', 0,  0,  0,  2,  0,  '', 1,  ''),
-(6, 'Weekend trading',  'Nu tar jag helg. JorÃ¥satte.', 'Borgargatan 8',  12345,  'Hornstull',  0,  0,  1454630400, 1,  2,  'GÃ¤rna en chokladkaka.', 1,  ''),
-(7, 'Hannah Ã¤r smart', 'She apparently grew a brain at last',  '', 0,  '', 0,  0,  1452902400, 1,  1,  'Amanda', 0,  ''),
-(8, 'Kaffekopp sökes',  'Jag är kaffesugen och behöver mer koffein. Helst för en halvtimme sedan. ',  'Pelargatan 4', 68432,  'Munkfors', 0,  1453051459, 1453656259, 1,  1,  '', 1,  '71ETlOSCNlL._SL1500__1453053219.jpg'),
-(9, 'Ã„nnu en annons',  'hej hej hej hej hej',  'Pelargatan 4', 68432,  'Munkfors', 0,  0,  1455148800, 1,  0,  '', 1,  ''),
-(10,  'hejhej', 'hejehjeheejjej jkjkj', 'Pelargatan 4', 68432,  'Munkfors', 0,  0,  1455235200, 1,  0,  '', 0,  ''),
-(12,  'testar dropdown',  'hej hej hello',  'Pelargatan 4', 68432,  'Munkfors', 1451992722, 0,  1452556800, 1,  0,  '', 0,  ''),
-(14,  'testar igen',  'hej hej hej hej hej hej hej hej hej hej hej hej hej hej hej hej hej hej hej hej hej hej hej hej hej hej hej hej och sÃ¥ vidare', 'Pelargatan 4', 68432,  'Munkfors', 1452075801, 0,  1452556800, 1,  0,  '', 0,  ''),
-(15,  'Litet test igen',  'Teeest test test Teeest test test Teeest test test Teeest test test Teeest test test Teeest test test Teeest test test Teeest test test Teeest test test Teeest test test Teeest test test .', 'Pelargatan 4', 68432,  'Munkfors', 1452092204, 0,  1452643200, 1,  3,  '', 0,  ''),
-(17,  'Kaffe bytes',  'hjhjs ashdjashdjasd askasjdhjsad \r\nhej hej', 'PingstvÃ¤gen 34',  12636,  'HÃ¤gersten', 1452250035, 0,  1453248000, 3,  3,  'Te', 1,  '');
+INSERT INTO `ads` (`id`, `title`, `content`, `address_street`, `address_zip`, `address_city`, `date_created`, `date_updated`, `date_expire`, `user_id`, `ad_type`, `payment`, `active`, `image`, `latitude`, `longitude`) VALUES
+(3, 'Test ad 3',  'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ornare lacinia sapien sed congue. Suspendisse dignissim eros non nisl rhoncus convallis. Sed consequat venenatis lobortis. Sed luctus auctor nisl ut posuere. Mauris tincidunt maximus risus, ac dignissim ipsum dignissim eu. Integer pharetra leo nec luctus cursus. Nunc ut lobortis nulla, ac pulvinar massa. Phasellus sit amet arcu a metus accumsan egestas id non ante. Aenean at massa dapibus, convallis quam in, cursus metus. Nunc venenatis ante eu congue porta. Praesent ac velit a massa viverra sollicitudin. Sed accumsan dolor sed turpis tincidunt facilisis. Phasellus ut rutrum tortor, quis volutpat odio. Donec porta purus nunc, in faucibus sem hendrerit eu.',  '', 0,  '', 0,  0,  0,  2,  0,  '', 1,  '', '', ''),
+(6, 'Weekend trading',  'Nu tar jag helg. JorÃ¥satte.', 'Pelargatan 4', 12147,  'Johanneshov',  0,  0,  1454630400, 1,  2,  'GÃ¤rna en chokladkaka.', 1,  '', '59.2953531', '18.0866078'),
+(7, 'Hannah Ã¤r smart', 'She apparently grew a brain at last',  '', 0,  '', 0,  0,  1452902400, 1,  1,  'Amanda', 0,  '', '', ''),
+(8, 'Kaffekopp sökes',  'Jag är kaffesugen och behöver mer koffein. Helst för en halvtimme sedan. ',  'Båtvägen 1', 68432,  'Munkfors', 0,  1453051459, 1453656259, 1,  1,  '', 1,  '71ETlOSCNlL._SL1500__1453053219.jpg',  '59.820034',  '13.5178342'),
+(9, 'Ã„nnu en annons',  'hej hej hej hej hej',  'Pelargatan 4', 68432,  'Munkfors', 0,  0,  1455148800, 1,  0,  '', 1,  '', '', ''),
+(10,  'hejhej', 'hejehjeheejjej jkjkj', 'Pelargatan 4', 68432,  'Munkfors', 0,  0,  1455235200, 1,  0,  '', 0,  '', '', ''),
+(12,  'testar dropdown',  'hej hej hello',  'Pelargatan 4', 68432,  'Munkfors', 1451992722, 0,  1452556800, 1,  0,  '', 0,  '', '', ''),
+(14,  'testar igen',  'hej hej hej hej hej hej hej hej hej hej hej hej hej hej hej hej hej hej hej hej hej hej hej hej hej hej hej hej och sÃ¥ vidare', 'Pelargatan 4', 68432,  'Munkfors', 1452075801, 0,  1452556800, 1,  0,  '', 0,  '', '', ''),
+(15,  'Litet test igen',  'Teeest test test Teeest test test Teeest test test Teeest test test Teeest test test Teeest test test Teeest test test Teeest test test Teeest test test Teeest test test Teeest test test .', 'Pelargatan 4', 68432,  'Munkfors', 1452092204, 0,  1452643200, 1,  3,  '', 0,  '', '', ''),
+(17,  'Kaffe bytes',  'hjhjs ashdjashdjasd askasjdhjsad \r\nhej hej', 'PingstvÃ¤gen 34',  12636,  'HÃ¤gersten', 1452250035, 0,  1453248000, 3,  3,  'Te', 1,  '', '', '');
 
 DROP TABLE IF EXISTS `ad_has_tag`;
 CREATE TABLE `ad_has_tag` (
@@ -51,6 +53,8 @@ INSERT INTO `ad_has_tag` (`ad_id`, `tag_id`) VALUES
 (17,  3),
 (18,  2),
 (18,  5),
+(8, 6),
+(8, 3),
 (8, 5);
 
 DROP TABLE IF EXISTS `ad_types`;
@@ -112,7 +116,7 @@ CREATE TABLE `user` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 INSERT INTO `user` (`id`, `firstname`, `lastname`, `address_city`, `address_street`, `address_zip`, `email`, `phone`, `password`, `premium`) VALUES
-(1, 'Per',  'i Hagen',  'Munkfors', 'Pelargatan 4', 68432,  'nacho@taco.com', 0,  '58ecff21ea6428c821b117958339a14fcfe63320', 0),
+(1, 'Per',  'i Hagen',  'Johanneshov',  'Pelargatan 4', 12147,  'nacho@taco.com', 0,  '58ecff21ea6428c821b117958339a14fcfe63320', 0),
 (3, 'Sara', 'Holm', 'HÃ¤gersten', 'PingstvÃ¤gen 34',  12636,  'saraholm82@yahoo.se',  0,  '831a86811ae88b637ac51e8769e4d2b52e537405', 0),
 (4, '', '', '', '', 0,  '', 0,  'c8d5a74c897c8d7406c1c3c01c657b5c12515b52', 0),
 (5, 'Thomas', 'Kjellberg',  'Jönköping',  'Skuggaliden 1',  55312,  'thomas.kjellberg@gmail.com', 0,  '831a86811ae88b637ac51e8769e4d2b52e537405', 0),
@@ -145,4 +149,4 @@ INSERT INTO `user_interested_in_ad` (`ad_id`, `user_id`, `date`, `denied`) VALUE
 (6, 7,  1452782475, 0),
 (6, 6,  1452782475, 0);
 
--- 2016-01-17 18:07:02
+-- 2016-01-18 09:18:05
