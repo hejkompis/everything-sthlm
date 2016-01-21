f72323e96fff8c22eb723d558a1583c914207f8a
Per Olsson <per.olsson@gmail.com>
2016-01-21 21:52:25 +0100

    Demo of distance slider with jquery ui on ads.getallads.twig. Implementation of jqueryui.css and js.

--- js/functions.js
+++ js/functions.js
@@ -14,4 +14,40 @@ $(document).ready(function() {
 
 	});
 
+	$(function() {
+
+		$( "#search-distance" ).slider({
+			range: "min",
+			min: 100,
+			max: 10000,
+			value: 10000,
+			slide: function( event, ui ) {
+				$( "#search-distance-input" ).val( ui.value );
+				distance = distancePrinter(ui.value);
+				$( "#search-distance-text" ).text( distance );
+			}
+		});
+
+		$( "#search-distance-input" ).val( $( "#search-distance" ).slider( "value" ) );
+
+	});
+
 });
+
+function distancePrinter(distance) {
+
+	if(distance > 9999) {
+		printed_distance = 'Ingen begränsning';
+	}
+	else if(distance < 10000 && distance > 999) {
+		printed_distance = (distance / 1000).toFixed(1) + ' kilometer';
+	}
+	else {
+		printed_distance = distance + ' meter';
+	}
+
+	console.log(distance);
+
+	return printed_distance;
+
+}
